cfbe507eeb812d19127de5eebccc8567e5fa1a72
Yin-Jui <ckjimmy634@gmail.com>
2019-07-25 16:55:05 +0800

    Add asyncTask for internet connection

--- app/src/main/java/com/example/shop/ParkingActivity.kt
+++ app/src/main/java/com/example/shop/ParkingActivity.kt
@@ -6,19 +6,34 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_parking.*
+import org.jetbrains.anko.*
 import java.net.URL
 
 
-class ParkingActivity : AppCompatActivity() {
+class ParkingActivity : AppCompatActivity(), AnkoLogger {
     private val TAG = ParkingActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking)
         val parking = "http://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f"
-        ParkingTask().execute(parking)
+   //     ParkingTask().execute(parking)
 
+        doAsync {
+            val url = URL(parking)
+            val json = url.readText()
+            info(json)
+            uiThread {
+                //  Toast.makeText(it,"GOT IT",Toast.LENGTH_LONG)
+                toast("GOT IT")
+                infor.text = json
+                alert ("GOT IT","ALERT"){
+                    okButton {  }
+                }.show()
+            }
+        }
     }
+
 
     inner class ParkingTask : AsyncTask<String,Void,String>() {
         override fun doInBackground(vararg params: String?): String {
@@ -32,7 +47,7 @@ class ParkingActivity : AppCompatActivity() {
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             Toast.makeText(this@ParkingActivity,"GOT IT",Toast.LENGTH_LONG).show()
-            info.text = result
+            infor.text = result
         }
     }
 }
