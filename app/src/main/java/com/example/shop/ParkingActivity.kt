fbe12a8d0be71278f031ead5bdaf551b55dbfbaa
Yin-Jui <ckjimmy634@gmail.com>
2019-07-25 16:56:49 +0800

    parking activity extends AnkoLogger

--- app/src/main/java/com/example/shop/ParkingActivity.kt
+++ app/src/main/java/com/example/shop/ParkingActivity.kt
@@ -19,6 +19,7 @@ class ParkingActivity : AppCompatActivity(), AnkoLogger {
         val parking = "http://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f"
    //     ParkingTask().execute(parking)
 
+        //Another way to create thread
         doAsync {
             val url = URL(parking)
             val json = url.readText()
